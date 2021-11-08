f6d26fe92a14820230dd3deab4df24b13ba5f994
wlj <331911962@qq.com>
2021-11-08 11:27:47 +0800

    push commit2

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -8,5 +8,7 @@ public class GitTest {
         System.out.println("hahashahahas4");
         System.out.println("hahashahahas5");
         System.out.println("hahashahahas88");
+        System.out.println("push hahahah99");
+
     }
 }
